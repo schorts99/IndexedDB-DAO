9ee15f040fa2b6af4aadd5b74b65585d9f755e60
schorts99 <schorts99@gmail.com>
2026-05-25 20:05:33 +0000

    feat: re-export EntityRegistry.

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from "./exceptions";
 export { IndexedDBDAO } from "./indexed-db.dao";
 export { IndexedDBUnitOfWorkRunner } from "./indexed-db.unit-of-work-runner";
 export { IndexedDBUnitOfWork } from "./indexex-db.unit-of-work";
+export { EntityRegistry } from "@schorts/shared-kernel";
